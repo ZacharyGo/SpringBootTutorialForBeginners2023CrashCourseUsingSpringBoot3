a2fa13680dc5e00a4fa38ca508a0b94f2b55369a
Zachary-Fujitsu <zsgoempire@gmail.com>
2023-06-17 00:10:41 +0800

    Spring Initializer

---
+++ src/main/java/dev/zach/contentcalendar/ContentCalendarApplication.java
@@ -0,0 +1,14 @@
+package dev.zach.contentcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentCalendarApplication.class, args);
+	}
+
+}
+ 
