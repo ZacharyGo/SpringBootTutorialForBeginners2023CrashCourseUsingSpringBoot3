26d3ce6f9cce7a5bc2378287b997dc638d5fde0c
Zachary-Fujitsu <zsgoempire@gmail.com>
2023-06-17 00:27:37 +0800

    Bean - Add Component Message

---
+++ src/main/java/dev/zach/contentcalendar/Message.java
@@ -0,0 +1,11 @@
+package dev.zach.contentcalendar;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Message {
+	public String getMessage() {
+		return "Hello World";
+	}
+
+}
